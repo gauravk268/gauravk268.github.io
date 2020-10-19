0ffef1c6f88311624d941751c56fa84890a31de6
gauravk268 <gauravk26800@gmail.com>
2020-10-19 23:46:54 +0530

    - updated read more in projects - added alerts on github redirect - changed brand color - corrected no icon in navbar - corrected padding of all elements

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var cert = document.getElementById("certificates");
+var open_cert = -1;
 cert.style.display = "none";
-var open = -1;
 
-function showCert(){
-	if(open == -1)
-	{
+function showCert() {
+    if (open_cert == -1) {
+        cert.style.display = "block";
+        open_cert = open_cert ^ 1;
+    } else {
+        cert.style.display = "none";
+        open_cert = open_cert ^ 1;
+    }
+}
 
-		cert.style.display = "block";
-		open = open^1;
-	}
-	else
-	{
-		cert.style.display = "none";
-		open = open^1;
-	}
+var show_more = -1;
+function moreLed() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("moreLed");
+
+    if (show_more == -1) {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "block";
+        show_more = show_more^1;
+    } else {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        show_more = show_more^1;
+    }
 }
