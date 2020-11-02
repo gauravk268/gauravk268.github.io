315388b514a880c525cf88562cf5bea8752d6f57
gauravk268 <gauravk26800@gmail.com>
2020-11-02 11:49:14 +0530

    - added keywords for better SEO - updated changes to main index

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
+var cert = document.getElementById("certificates");
+var open_cert = -1;
+cert.style.display = "none";
+
 function expandProjects(){
     document.getElementById("dropdown").style.display = "block";
 }
 
 function shrinkProjects(){
     document.getElementById("dropdown").style.display = "none";
-}
-
-var cert = document.getElementById("certificates");
-var open_cert = -1;
-cert.style.display = "none";
-
-function showCert() {
-    if (open_cert == -1) {
-        cert.style.display = "block";
-        open_cert = open_cert ^ 1;
-    } else {
-        cert.style.display = "none";
-        open_cert = open_cert ^ 1;
-    }
 }
 
 var show_more = -1;
@@ -29,12 +19,22 @@ function moreLed() {
     if (show_more == -1) {
         dots.style.display = "none";
         btnText.innerHTML = "Read less";
-        moreText.style.display = "block";
+        moreText.style.display = "inline";
         show_more = show_more^1;
     } else {
         dots.style.display = "inline";
         btnText.innerHTML = "Read more";
         moreText.style.display = "none";
         show_more = show_more^1;
+    }
+}
+
+function showCert() {
+    if (open_cert == -1) {
+        cert.style.display = "block";
+        open_cert = open_cert ^ 1;
+    } else {
+        cert.style.display = "none";
+        open_cert = open_cert ^ 1;
     }
 }
