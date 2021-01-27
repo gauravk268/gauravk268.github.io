842cc4200de7767fcd0c9d67bc7a3654ec8581db
Gaurav Kumar <gauravk26800@gmail.com>
2021-01-27 17:11:32 +0530

    added heroku procfile and fixed port value for heroku

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', (req, res)=>{
   res.status(200).sendFile(__dirname + './index.html');
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.port || 3000, ()=>{
   console.log("Server up and running on port 3000!!");
 });
