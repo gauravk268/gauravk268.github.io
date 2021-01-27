73223ea53c83fead17ef1ad628cc2b4af99065c5
Gaurav Kumar <gauravk26800@gmail.com>
2021-01-27 17:48:57 +0530

    added path for favicon.ico

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res)=>{
   res.status(200).sendFile(__dirname + './index.html');
 });
 
+app.get('/favicon.ico', (req, res)=>{
+  res.status(204);
+  res.redirect('/');
+});
+
 app.listen(process.env.port || 3000, ()=>{
   console.log("Server up and running!!");
   console.log(process.env.port);
