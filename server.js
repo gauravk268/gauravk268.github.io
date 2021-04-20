d3cf7553e7192081b9d0553a53333d6a614946ab
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-21 00:56:42 +0530

    adding file for netlify updated server.js port

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const connectDB = function () {
   const uri =
@@ -35,51 +34,52 @@ const feedbackSchema = new mongoose.Schema({
   email: String,
   message: String,
   localTimeStamp: String,
-  timeStamp: { type : Date, default: Date.now }
+  timeStamp: { type: Date, default: Date.now },
 });
 
-app.post('/feedback', (req, res)=>{
+app.post("/feedback", (req, res) => {
   const FeedBack = mongoose.model("FeedBack", feedbackSchema);
 
-  const localTimeDate = new Date().toLocaleString("en-US", { timeZone: 'Asia/Kolkata' }).
-    replace(/\//, '-').
-    replace(/\//, '-');
-  
+  const localTimeDate = new Date()
+    .toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+    .replace(/\//, "-")
+    .replace(/\//, "-");
+
   const feedback = new FeedBack({
     name: req.body.name,
     email: req.body.email,
     message: req.body.feedback,
-    localTimeStamp: "mm-dd-yyyy: " + localTimeDate
+    localTimeStamp: "mm-dd-yyyy: " + localTimeDate,
   });
 
   feedback.save(function (err, savedfeedback) {
     if (err) {
       console.log("Could not save Feedback: " + err);
-      res.send("<h2>Could not save your feedback. Please try again.</h2>")
+      res.send("<h2>Could not save your feedback. Please try again.</h2>");
     } else {
       // console.log(savedfeedback);
       console.log("[mongoose] Feedback saved successfully.");
-      res.status(200).sendFile(__dirname + "/public/feedback_success.html")
+      res.status(200).sendFile(__dirname + "/public/feedback_success.html");
     }
   });
 });
 
-app.post("/homepage", (req, res)=>{
+app.post("/homepage", (req, res) => {
   res.redirect("/");
 });
 
-
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
   // res.status(200).sendFile(__dirname + './index.html');
-  res.status(200).sendFile(__dirname + './index.min.html');
+  res.status(200).sendFile(__dirname + "./index.min.html");
 });
 
-app.get('/favicon.ico', (req, res)=>{
+app.get("/favicon.ico", (req, res) => {
   res.status(204);
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, ()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server up and running!!");
-  console.log(process.env.PORT);
+  console.log(PORT);
 });
